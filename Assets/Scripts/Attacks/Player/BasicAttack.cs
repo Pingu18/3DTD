35897627de1032875cb0f050bb74c1651452af43
Jason Leung <40867693+Hareye@users.noreply.github.com>
2022-05-12 14:11:38 -0700

    update
    - idk what these updates are tbh

--- Assets/Scripts/Attacks/Player/BasicAttack.cs
+++ Assets/Scripts/Attacks/Player/BasicAttack.cs
@@ -9,22 +9,6 @@ public class BasicAttack : MonoBehaviour
 
     private GameObject effectToSpawn;
 
-    /*
-    private PlayerController playerController;
-
-    [SerializeField] private GameObject currentAttackContainer;
-    [SerializeField] private Rigidbody currentRigidbody;
-
-    [SerializeField] private GameObject fireAttackContainer;
-
-    [SerializeField] private ParticleSystem collisionParticleSystem;
-
-    private List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
-
-    [Header("Player Orientation")]
-    [SerializeField] private Transform orientation;     // reference to player orientation
-    */
-
     void Start()
     {
         //playerController = GetComponent<PlayerController>();
@@ -43,24 +27,9 @@ public class BasicAttack : MonoBehaviour
     }
 
     /*
-    public void fireBasicAttack()
-    {
-        for (int i = 0; i < fireAttackContainer.transform.childCount; i++)
-        {
-            ParticleSystem currentParticleSystem = fireAttackContainer.transform.GetChild(i).gameObject.GetComponent<ParticleSystem>();
-
-            currentParticleSystem.Play();
-        }
-    }
-
     private void OnParticleCollision(GameObject other)
     {
-        for (int i = 0; i < currentAttackContainer.transform.childCount; i++)
-        {
-            ParticleSystem currentParticleSystem = currentAttackContainer.transform.GetChild(i).gameObject.GetComponent<ParticleSystem>();
-
-            currentParticleSystem.Stop();
-        }
+        
     }
     */
 }
